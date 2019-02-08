b6c5f00fdeab694321df54b7e49a4e5881a90ca6
Cameron Hudson <35085463+CameronHudson8@users.noreply.github.com>
2019-02-07 22:07:12 -0800

    Replaced explicit component link with router outlet and route.

--- my-app/src/app/app-routing.module.ts
+++ my-app/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MyFirstComponentComponent } from './components/my-first-component/my-first-component.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: MyFirstComponentComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
